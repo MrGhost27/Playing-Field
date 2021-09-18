26b78297e5a20d91e240528effd373877386bc6c
Ghost <Ghost27@live.co.uk>
2021-09-18 21:11:23 +0100

    First commit

---
+++ Playing Field/Assets/TileManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    // The manager doesn't care about spawning tiles.
+    TileSpawner TS;
+    
+    // Which tile prefab will be passed to the spawner.
+    [SerializeField] private GameObject Tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Adding it, you can't customise it.
+        //TS = gameObject.AddComponent<TileSpawner>();
+        TS = gameObject.GetComponent<TileSpawner>();
+        if (TS==null) 
+        {
+            Debug.Log("There is no TileSpawner attached to this gameobject: " + gameObject.name);
+        }
+        InitialiseSpawner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitialiseSpawner()
+    {
+        TS.ResetSize(3, 3);
+        if (Tile== null)
+        {
+            Debug.Log("You don't have a tile!");
+        }
+        else {
+            TS.SetPrefab(Tile);
+            TS.SpawnTiles();
+        }
+    }
+}
---
+++ Playing Field/Assets/TileSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    public int width = 1;
+    public int depth = 1;
+
+    GameObject TilePrefab;
+
+    public void SpawnTiles()
+    {
+        Debug.Log("Spawning the tiles. Width: " + width + " depth: " + depth);
+    }
+
+    public void ResetSize(int newWidth, int newDepth)
+    {
+        width = newWidth;
+        depth = newDepth;
+    }
+
+    public void SetPrefab(GameObject GO) {
+        TilePrefab = GO;
+    }
+}
